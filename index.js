8c70e9550fef799157f1b86fad672313ce4a51c8
Mai Joud <mayajoud.123456@gmail.com>
2025-02-21 14:19:15 +0330

    fix: تعديل إعدادات السيرفر

--- index.js
+++ index.js
@@ -52,3 +52,29 @@ module.exports = {
   Chain,
   Relation,
 };
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// استخدم المنفذ الذي توفره المنصة أو 5000 للتشغيل المحلي
+const PORT = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, "static"); 
+
+
+
+// تأكد من أن المسار الذي يضع فيه الملفات الثابتة صحيح
+console.log("🛠 المسار الصحيح للملفات الثابتة:", publicPath);
+app.use(express.static(publicPath));
+
+// إرسال index.html عند الوصول إلى الجذر
+app.get("/", (req, res) => {
+  const filePath = path.join(publicPath, "index.html");
+  console.log("📄 محاولة إرسال الملف:", filePath);
+  res.sendFile(filePath);
+});
+
+// تشغيل السيرفر
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(🚀 Server running on port ${PORT});
+});
